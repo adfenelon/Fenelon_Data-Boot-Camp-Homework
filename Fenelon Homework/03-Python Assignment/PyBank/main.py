538c3dc886582a2f7c411ab84e60a79684c147b0
Alexandra Fenelon <alexandra.fenelon1122@gmail.com>
2019-06-30 00:50:22 -0400

    Python HW update

--- Fenelon Homework/03-Python Assignment/PyBank/main.py
+++ Fenelon Homework/03-Python Assignment/PyBank/main.py
@@ -4,11 +4,10 @@ Created on Sat Jun 29 23:07:09 2019
 
 @author: fenel
 """
-
-import csv
 import os
+import csv
 
-bankcsvfile = "bank_data.csv"
+budget_csvpath = os.path.join("..", "Resources", "budget_data.csv")
 
 monthly_tally=0
 profit_loss=0
@@ -16,12 +15,12 @@ previous_profit=0
 profit_list=[]
 month_list=[]
 
-with open(bankcsvpath, "r") as datafile:
-    bankreader = csv.bankreader(datafile)
+with open(budget_csvpath, "r") as csvfile:
+    budgetreader = csv.reader(csvfile, delimiter=",")
     
     header = next(bankreader)
     
-    for row in bankreader:
+    for row in budgetreader:
         month_tally +=1
         profit_loss += float(row[1])
         current_profit= float(row[1])
@@ -32,4 +31,17 @@ with open(bankcsvpath, "r") as datafile:
 
     average_change = sum(profit_list)/85
     max_profit = max(profit_list)
-    max_month = month_list
+    max_month = month_list[profit_list.index(max_profit)]
+    min_profit = min(profit_list)
+    min_month = month_list[profit_list.index(min_profit)]
+    
+Print("Total Months: " + str(month_tally))
+print("Total: " + "$" + str(profit_loss))
+print("Average Change:" + str(average_change))
+print("Greatest Increase in Profits:" + str(max_month) + "$" + str(max_profit))
+print("Greatest Deacrease in Profits:" + str(min_month) + "$" + str(min_profit))
+
+bankoutput_file = os.path.join("bank_summary.csv")
+
+with open(bankoutput_file, "w", newline="") as datafile:
+    writer = csv.writer(datafile)
--- Fenelon Homework/03-Python Assignment/PyPoll/main.py
+++ Fenelon Homework/03-Python Assignment/PyPoll/main.py
@@ -5,3 +5,87 @@ Created on Sat Jun 29 23:03:48 2019
 @author: fenel
 """
 
+
+## PyPoll
+
+#![Vote-Counting](Images/Vote_counting.png)
+
+#* In this challenge, you are tasked with helping a small, rural town modernize its vote-counting process. (Up until now, Uncle Cleetus had been trustfully tallying them one-by-one, but unfortunately, his concentration isn't what it used to be.)
+
+#* You will be give a set of poll data called [election_data.csv](PyPoll/Resources/election_data.csv). The dataset is composed of three columns: `Voter ID`, `County`, and `Candidate`. Your task is to create a Python script that analyzes the votes and calculates each of the following:
+
+import os
+import csv
+
+Voter_ID_Tally = 0
+Summary = "Election Results"
+Blank = "------------------------------"
+
+electioncsvpath = os.path.join('Resources','election_data.csv')
+
+with open(electioncsvpath, "r") as datafile:
+    election_reader = csv.reader(datafile, delimiter=',')
+    header = next(election_reader)
+
+    Candidates = []
+
+#* The total number of votes cast
+    for row in election_reader:
+        Candidates.apend(row[2])
+        
+    Total_Votes = len(Candidates)
+
+print("Total Votes:" + Str(Voter_ID_Tally))
+
+print(Blank)
+
+print(Summary)
+
+print(blank)
+
+#* A complete list of candidates who received votes
+candidates_unique = list(set(Candidates)
+
+
+#* The percentage of votes each candidate won
+percent = round(int(row[3]) / int(row[5]), 2)
+        review_percent.append(percent)
+#* The total number of votes each candidate won
+#* The winner of the election based on popular vote.
+
+print(Summary)
+print(Blank)
+print(":" + Str(Voter_ID_Tally))
+print(Blank)
+Print(":" + Str() +)
+Print(":" + Str() +)
+Print(":" + Str() +)
+Print(":" + Str() +)
+Print(Blank)
+Print("Winner: ")
+Print(Blank)
+
+
+polloutput_file = os.path.join("election_summary.csv")
+
+with open(polloutput_file, "w", newline="") as datafile:
+    writer = csv.writer(datafile)
+
+
+
+
+
+#Expected Output :#* As an example, your analysis should look similar to the one below:
+
+    #  ```text
+    #  Election Results
+    #  -------------------------
+    #  Total Votes: 3521001
+    #  -------------------------
+    #  Khan: 63.000% (2218231)
+    #  Correy: 20.000% (704200)
+    #  Li: 14.000% (492940)
+    #  O'Tooley: 3.000% (105630)
+    #  -------------------------
+    #  Winner: Khan
+    #  -------------------------
